e24589c7b9012465db4f8e8fafb920f83cb71180
Kaustubh Naithani <74718102+Kaustubh101907@users.noreply.github.com>
2022-05-14 23:43:16 +0530

    Add files via upload

---
+++ src/LinkedList/SinglyLinkedList/DeleteNthNodeFromTheEnd.java
@@ -0,0 +1,40 @@
+package LinkedList.SinglyLinkedList;
+
+import java.util.List;
+
+public class DeleteNthNodeFromTheEnd {
+    public static void main(String[] args) {
+        ListNode head=new ListNode(10);
+        head.next=new ListNode(20);
+        head.next.next=new ListNode(30);
+        head.next.next.next=new ListNode(40);
+        head.next.next.next.next=new ListNode(50);
+        printlist(head);
+        head = deleteNthFromEnd(head,2);
+        printlist(head);
+    }
+
+    public static ListNode deleteNthFromEnd(ListNode head, int n) {
+        int len = 0;
+        ListNode curr = head;
+        while (curr.next != null) {
+            len++;
+            curr = curr.next;
+        }
+        curr = head;
+        for (int i = 1 ; i < len - n + 1; i++) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+
+    public static void printlist (ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
