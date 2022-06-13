6e98fd83f646da169934d4dee15e322a5c2b74da
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-13 21:19:21 +0530

    Learning DSA

---
+++ src/Recursion/IsArraySortedOrNot.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class IsArraySortedOrNot {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,7,5,6,7};
+        int i = 0;
+        System.out.println(isSorted(arr, i));
+    }
+
+    private static boolean isSorted(int[] arr, int i) {
+        if (i == arr.length -1) {
+            return true;
+        }
+        if (arr[i] > arr[i+1]) {
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+}
