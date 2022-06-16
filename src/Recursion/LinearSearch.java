b100d28a7b8019578aab5f3b96739f978f0a4208
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-16 21:14:10 +0530

    Learning DSA

--- src/Recursion/LinearSearch.java
+++ src/Recursion/LinearSearch.java
@@ -2,11 +2,22 @@ package Recursion;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {3,2,1,18,9};
-        int target = 9;
+        int[] arr = {3,2,1,18,18,9};
+        int target = 18;
         int i = 0;
         System.out.println(isPresent(arr, target, i));
         System.out.println(linearSearch(arr, target, i));
+        linearSearchAllIndex(arr, target, i);
+    }
+
+    private static void linearSearchAllIndex(int[] arr, int target, int i) {
+        if (i == arr.length) {
+            return;
+        }
+        if (arr[i] == target) {
+            System.out.print(i + " ");
+        }
+        linearSearchAllIndex(arr, target, i+1);
     }
 
     private static boolean isPresent(int[] arr, int target, int i) {
