6120af4459bbdee97cf2856a7092574f11cd5edf
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-18 21:15:20 +0530

    Learning DSA

--- src/JavaLibraries/MapInterface/MapInterfaceExample.java
+++ src/JavaLibraries/MapInterface/MapInterfaceExample.java
@@ -8,6 +8,7 @@ public class MapInterfaceExample {
         m.put(100, "GFG");
         m.put(200, "courses");
         m.put(150, "IDE");
+        m.put(100, "haha"); // changes the 100 value to haha
         System.out.println(m);
     }
 }
