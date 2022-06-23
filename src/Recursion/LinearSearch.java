52df5460786f7ff71307696e3e14b537c0afe95c
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-23 14:44:25 +0530

    Learning DSA

--- src/Recursion/LinearSearch.java
+++ src/Recursion/LinearSearch.java
@@ -4,16 +4,19 @@ import java.util.*;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {3,2,1,18,18,9};
+        int[] arr = {2, 3, 1, 4, 4, 5};
         int target = 18;
         int i = 0;
-        System.out.println(isPresent(arr, target, i));
-        System.out.println(linearSearch(arr, target, i));
-        linearSearchAllIndex(arr, target, i);
-        System.out.println();
-        findAllIndex(arr, target, i);
+//      System.out.println(isPresent(arr, target, i));
+//      System.out.println(linearSearch(arr, target, i));
+//      linearSearchAllIndex(arr, target, i);
+//      System.out.println();
+//      findAllIndex(arr, target, i);
+//      System.out.println(list);
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> ans = findAllIndex(arr, 4, 0, list);
+        System.out.println(ans);
         System.out.println(list);
-        ArrayList<Integer> al = linearSearchAllIndexArrayList(arr, target, i, new ArrayList<>());
     }
 
     private static void linearSearchAllIndex(int[] arr, int target, int i) {
@@ -54,13 +57,13 @@ public class LinearSearch {
         findAllIndex(arr, target, index + 1);
     }
 
-    private static ArrayList linearSearchAllIndexArrayList(int[] arr, int target, int i, ArrayList<Integer> al) {
-        if (i == arr.length) {
-            return al;
+    private static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
         }
-        if (arr[i] == target) {
-            al.add(i);
+        if (arr[index] == target) {
+            list.add(index);
         }
-        return linearSearch(arr, target, i+1, al);
+        return findAllIndex(arr, target, index + 1, list);
     }
 }
