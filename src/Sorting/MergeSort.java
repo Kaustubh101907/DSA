6acb50f6762a28587316ce42adcb81d430f7881d
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-07-10 21:08:56 +0530

    Learning DSA

--- src/Sorting/MergeSort.java
+++ src/Sorting/MergeSort.java
@@ -1,7 +1,52 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
+        int[] arr = {5,6,4,7,2,1,3};
+        int[] ans = mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+    }
 
+    private static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int mid = arr.length /2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] arr = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                arr[k] = nums1[i];
+                i++;
+                k++;
+            } else {
+                arr[k] = nums2[j];
+                j++;
+                k++;
+            }
+        }
+        while (i < m) {
+            arr[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while (j < n) {
+            arr[k] = nums2[j];
+            j++;
+            k++;
+        }
+        return arr;
     }
 }
